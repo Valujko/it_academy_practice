d3a6faa95a25b291ea4f011f103139a9effb9314
valujko <valujko@gmail.com>
2019-03-12 20:06:39 +0300

    Test

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.TimeSpan timeSpan = DateTime.Now.TimeOfDay;
+
+            if ((timeSpan.Hours >= 9) && (timeSpan.Hours <= 9))
+            {
+                Console.WriteLine("Good morning, guys");
+            }
+            else if ((timeSpan.Hours > 12) && (timeSpan.Hours < 15))
+            {
+                Console.WriteLine("Good day, guys");
+            }
+            else Console.WriteLine("Good evening, guys");
+
+            Console.ReadKey();
+        }
+    }
+}
